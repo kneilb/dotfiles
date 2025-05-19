4b371e7c661be454fe4d80880a86b81665fc9d18
Neil Burningham <neil.burningham@mediakind.com>
2025-05-19 17:28:31 +0100

    emacs: Add org-roam-ui

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -446,6 +446,19 @@
   :config
   (org-roam-db-autosync-mode))
 
+(use-package org-roam-ui
+  :pin melpa
+  :after org-roam
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
+
 ;; REST client stuff (verb is an extension of org)
 (use-package verb
   :pin melpa)
