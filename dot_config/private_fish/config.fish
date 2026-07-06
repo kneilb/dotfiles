3e7fdff59b3e520886b3f1fb0ce81e2b8754f2b3
Neil Burningham <kneilb@gmail.com>
2026-07-06 22:27:05 +0100

    config.fish: Add mise activate & opencode path

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,6 +8,10 @@ if status is-interactive
         fish_add_path $HOME/.pyenv/bin
         pyenv init - | source
     end
+
+    if type -q mise
+       mise activate fish | source
+    end
 end
 
 # user paths
@@ -15,6 +19,7 @@ fish_add_path $HOME/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/go/bin
+fish_add_path $HOME/.opencode/bin
 
 # user variables
 set -x KUBECONFIG $HOME/.kube/config:$HOME/.kube/kind-mkio-config:$HOME/.kube/az-aks-config:$HOME/.kube/aws-eks-config
