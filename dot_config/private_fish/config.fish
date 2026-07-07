69dd953b7773bc7f9f767326f0c120391cfadb9a
Neil Burningham <kneilb@gmail.com>
2026-07-07 09:04:44 +0100

    config.fish: remove pyenv & opencode path (managed with mise)

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,11 +4,6 @@ if status is-interactive
         atuin init fish | source
     end
 
-    if type -q $HOME/.pyenv/bin/pyenv
-        fish_add_path $HOME/.pyenv/bin
-        pyenv init - | source
-    end
-
     if type -q mise
        mise activate fish | source
     end
@@ -19,7 +14,6 @@ fish_add_path $HOME/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/go/bin
-fish_add_path $HOME/.opencode/bin
 
 # user variables
 set -x KUBECONFIG $HOME/.kube/config:$HOME/.kube/kind-mkio-config:$HOME/.kube/az-aks-config:$HOME/.kube/aws-eks-config
